ccae9ffd46a86c8ec9d852f6a3f854a6bc263ef4
ribbledrib <tobycraig126@gmail.com>
2026-02-10 15:49:28 -0400

    /

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
-    printf("Hello");
+    FILE *fptr;
+
+    fptr = fopen("data.txt", "r");
+
+    if (fptr == NULL){
+        printf("The file is not opened");
+    }
+    return 0;
 }
