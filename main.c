e9ff1caf3ded54297749a7a1206fe23845f4b933
ribbledrib <tobycraig126@gmail.com>
2026-03-07 15:43:47 -0400

    starting to read file for log in

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-int main(){
+char* open_file(){
     FILE *fptr;
 
-    fptr = fopen("data.txt", "r");
 
+    fptr = fopen("data.txt", "r");
+    
+    
+    char temp[255];
     if (fptr == NULL){
         printf("The file is not opened\n");
     }
     else{
         printf("The file was opened successfuly\n");
     }
-    return 0;
+    fclose(fptr);
+   char *buffer = malloc(strlen(temp) + 1);
+   if(buffer == NULL){
+    return NULL;
+   }
+   strcpy(buffer, temp);
+   return buffer;
+}
+int main(){
+
+    char *data = open_file();
+    if(data != NULL){
+        printf("%s\n", data);
+        free(data);
+    }
+    
+
 }
